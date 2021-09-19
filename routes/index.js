2e94495a04fb8d3ea98b37728d623b7a78866ef0
alemtani <alex.m.tanimoto@gmail.com>
2021-09-19 02:21:22 -0700

    Push changes for now

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,76 @@
 var express = require('express');
 var router = express.Router();
-// var axios = require('axios');
 
 var utils = require('../lib/utils');
 
-// Features we should use
-// 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 1.12, 2.2, 2.6, 4.1, 4.2
+// Imports the Google Cloud Prediction Service Client library
+const {PredictionServiceClient} = require('@google-cloud/aiplatform');
+
+// Specifies the location of the api endpoint
+const clientOptions = {
+  apiEndpoint: 'us-central1-aiplatform.googleapis.com',
+};
+
+// Instantiates a client
+const predictionServiceClient = new PredictionServiceClient(clientOptions);
+
+const project = '929297422851';
+const location = 'us-central1';
+const endpointId = '8197378154558390272';
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  const url = 'https://amazon.com';
+  const endpoint = `projects/${project}/locations/${location}/endpoints/${endpointId}`;
+  const url = 'https://spotify-personality-test.herokuapp.com';
   const domain = utils.get_domain(url);
-  const score = await utils.get_iframe(url);
-  console.log(score);
+
+  const scores = [];
+
+  scores.push(utils.validate_ip(domain));
+  scores.push(utils.get_length(url));
+  scores.push(utils.validate_tinyurl(domain));
+  scores.push(utils.contains_at(url));
+  scores.push(utils.locate_redirect(url));
+  scores.push(utils.contains_dash(url));
+  scores.push(utils.count_dots(url));
+
+  const httpsScore = utils.validate_https(url);
+
+  const myPromise = new Promise((resolve, reject) => {
+    if (httpsScore === -1) {
+      utils.verify_whois(domain, function(scores) {
+        resolve(scores);
+      });
+    } else {
+      resolve([1, 1, 1]);
+    }
+  });
+
+  myPromise
+  .then(result => {
+    scores.push(result[0]);
+    scores.push(result[1]);
+    scores.push(httpsScore);
+    scores.push(0);
+    scores.push(result[0]);
+    scores.push(0);
+    scores.push(result[2]);
+    scores.push(result[0]);
+  });
+
+  const anchorsScore = await utils.get_anchors(url, domain);
+  const iframeScore = await utils.get_iframe(url);
+
+  const instances = [scores];
+
+  const request = {
+    endpoint,
+    instances,
+  };
+
+  // Predict request
+  const [response] = await predictionServiceClient.predict(request);
+  console.log(response.predictions);
   res.render('index', { title: 'Express' });
 });
 
